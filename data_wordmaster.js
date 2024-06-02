d1817647f3980a4c3c1edf6a82412931f978a435
aawoon <aawoon@nate.com>
2024-06-02 14:32:34 +0900

    Update data_wordmaster.js

--- data_wordmaster.js
+++ data_wordmaster.js
@@ -1,3 +1,1281 @@
-let data = 
-`
+let word_string = 
+`Day 01 - Characters
+kind	친절한, 상냥한
+clever	영리한, 재치 있는
+wise	지혜로운, 슬기로운
+foolish	바보 같은, 어리석은
+proud	자랑스러워하는, 거만한, 잘난 체하는
+honest	정직한, 솔직한
+gentle	부드러운, 온화한
+careful	조심성 있는
+brave	용감한
+lazy	게으른
+funny	웃기는, 우스꽝스러운
+calm	차분한, 침착한
+character	성격, 기질
+serious	진지한, 진심의
+strict	엄한, 엄격한
+cruel	잔인한, 무자비한
+mean	야비한, 비열한
+selfish	이기적인
+evil	나쁜, 사악한, 악
+curious	호기심이 많은, 알고 싶어 하는
+cheerful	쾌활한, 명랑한
+friendly	친한, 다정한, 호의적인
+modest	겸손한, 신중한, 중도적인
+generous	관대한, (인심이) 후한
+sensitive	민감한, 예민한
+confident	자신만만한, 확신하는
+positive	긍정적인
+negative	부정적인
+optimistic	낙관적인, 낙천적인
+cautious	조심스러운, 신중한, 조심하는
+
+Day 02 - Describing People
+big	(몸집이) 큰
+old	나이가 많은, 늙은
+tall	키가 큰
+cute	귀여운, 예쁜
+pretty	예쁜, 귀여운, 매우, 아주
+beautiful	아름다운
+ugly	못생긴, 추한
+fat	뚱뚱한, 지방
+overweight	과체중의, 너무 살찐
+young	어린, 젊은
+handsome	(남자가) 잘 생긴
+slim	호리호리한, 가냘픈, 날씬한
+beard	턱수염
+plain	평범하게 생긴
+good-looking	잘 생긴
+skinny	깡마른, 바싹 여읜
+fit	건강한, 적합하다, 꼭 맞다
+muscular	근육질의, 건장한
+thin	가는, 가느다란, 여읜, 수척한, 숱이 적은
+bald	대머리의
+curly	곱슬거리는
+dye	염색하다
+appearance	외모
+attractive	매력적인
+charming	매력적인, 멋진
+mustache	코밑수염
+sideburns	구레나룻
+middle-aged	중년의
+build	체격, 짓다, 건축하다
+image	이미지, 상, 형태
+
+Day 03 - Emotions
+smile	미소, 미소 짓다
+enjoy	즐기다
+cry	울다
+tear	눈물
+glad	기쁜
+angry	화가 난
+fear	공포
+joy	기쁨, 즐거움
+miss	그리워하다, 놓치다
+laugh	웃다
+annoyed	짜증 난, 화가 난
+upset	화가 난, 기분이 상한
+worried	걱정스러운
+regret	유감, 후회, 후회하다
+bother	괴롭히다, 방해하다
+excited	흥분한, 신이 난
+surprised	놀란
+pleased	기쁜, 좋아하는
+horror	공포, 전율
+grateful	감사하는, 고맙게 여기는
+anxious	걱정되는, 근심이 되는
+delighted	매우 기뻐하는
+depressed	의기소침한, 낙담한, 우울한
+frightened	깜짝 놀란, 겁이 난
+ashamed	부끄러워하는
+emotion	감정
+sympathy	동정
+satisfied	만족한
+disappointed	실망한, 낙담한
+amused	즐기는, 즐거워하는
+
+Day 04 - Jobs
+baker	제빵사
+captain	선장
+reporter	기자, 통신원
+clerk	점원
+artist	예술가, 화가
+engineer	기사, 기술자
+scientist	과학자
+lawyer	변호사
+dentist	치과의사
+pilot	조종사, 조종하다
+officer	공무원, 관리
+gardener	정원사
+photographer	사진사
+president	대통령
+salesperson	판매원
+carpenter	목수
+businessman	사업가
+fisherman	어부
+soldier	군인
+professor	교수
+judge	판사, 심사원, 판단하다
+hairdresser	미용사
+accountant	회계사
+novelist	소설가
+security guard	경호원, 경비원
+teller	금전출납원
+astronaut	우주비행사
+detective	탐정
+secretary	비서
+illustrator	삽화가
+
+Day 05 - Clothes
+cap	(테가 없는) 모자
+pants	바지
+sweater	스웨터
+skirt	치마
+shoes	신발, 구두
+tie	넥타이, ~을 묶다, 매다
+T-shirt	티셔츠
+belt	벨트, 허리띠
+hat	(테가 있는) 모자
+uniform	유니폼, 제복
+socks	양말
+gloves	장갑
+boots	장화, 부츠, 목이 긴 구두
+dress	옷, 의복, 옷을 입다
+scarf	스카프, 목도리
+jacket	재킷, 상의, 웃옷
+shorts	반바지, 운동 팬츠
+button	단추, ~에 단추를 채우다
+jeans	청바지
+suit	정장, 슈트, ~에게 잘 어울리다
+pocket	주머니
+bow tie	나비넥타이
+heels	굽 높은 구두
+stockings	긴 양말, 스타킹
+sandals	샌들
+wallet	지갑
+purse	돈주머니, 지갑
+vest	조끼
+overalls	멜빵 바지
+athletic shoes	운동화
+
+Day 06 - Food
+milk	우유
+butter	버터
+bread	빵
+jam	잼
+meat	고기
+sugar	설탕
+salt	소금
+cookie	쿠키
+soup	수프
+fish    	생선
+jelly	젤리
+beef	쇠고기
+steak	스테이크
+pork	돼지고기
+pepper	후추, 고추
+diet	다이어트, 식이요법
+snack	간식
+egg	계란
+rice	밥
+flour	밀가루
+honey	꿀
+mustard	겨자
+noodle	국수
+pickle	(오이 따위 채소를) 절인 것, 피클, 절이다
+stew	스튜, 찌개
+cereal	곡물 식품, 시리얼, 곡물의
+meal	식사
+side dish	반찬, 주된 요리에 곁들이는 요리
+appetizer	애피타이저, 식욕을 돋우는 것
+powder	가루
+
+Day 07 - Cooking
+bake	굽다
+break	깨뜨리다
+fry	튀기다
+boil	끓이다
+mix	섞다
+oven	오븐
+glass	컵, 유리잔, 한 컵(의 양)
+knife	칼
+spoon	숟가락
+basket	바구니
+handle	손잡이
+pour	붓다
+roll	밀다
+slice	얇게 썰다
+refrigerator	냉장고
+pot	(속이 깊은) 냄비
+bowl	(우묵한) 그릇, 통
+plate	접시
+tray	쟁반
+jar	(입구가 넓은) 병, 단지
+pan  	(납작한) 냄비, 팬
+beat	휘저어 섞다
+steam	찌다
+scoop	주걱, 국자
+grill	(열로) 굽다, 익히다
+kettle	주전자
+opener	따개
+cabinet	진열대
+recipe	요리법
+blender	부엌용 믹서기
+
+Day 08 - House
+garden    	정원, 정원을 가꾸다
+kitchen	부엌
+apartment	아파트
+window	창문
+yard	마당
+knock	노크를 하다, (문 등을) 두드리다
+pet	애완동물
+soap	비누
+towel	타월, 수건
+curtain	커튼
+mirror	거울
+gate	문
+bedroom	침실
+roof	지붕
+garage	차고
+laundry	세탁물
+water	물, 물주다
+lawn	잔디
+floor	마루, 바닥, 층
+feed	먹이다
+bathroom	욕실
+ceiling	천장
+shelf	선반
+drawer	서랍
+lamp	전기스탠드, 램프
+sheet	시트, 홑이불
+stair	계단, 층계
+scale	체중계, 저울
+sink	싱크대, 개수대
+tap	수도꼭지
+
+Day 09 - Transportation
+come	오다, 도달하다, 도착하다
+go	가다
+park	주차하다, 공원
+stop	정류장, 정차하다, 그만두다
+drive	운전하다
+subway	지하철
+plane	비행기
+ticket	티켓, 표
+seat	좌석
+road	길, 도로
+fare	요금
+bicycle	자전거
+cross	~을 건너다, 십자가
+track	철도 선로, 궤도
+rail	철로
+curve	굽이, 커브, 굴곡, 구부러지다
+sign	표지(판), 서명하다
+station	역, 정거장, (관공)서, 국
+wheel	바퀴
+license	면허증
+accident	사고
+traffic	교통(량), 교통의
+forward	앞쪽으로, 앞을 향하여
+transfer	갈아타다, 나르다
+passenger	승객
+harbor	항구
+gas	휘발유, 가솔린
+platform	(역의) 플랫폼, 승강장
+transport	수송하다, 운송하다
+crash	충돌, 충돌하다
+
+Day 10 - In the Office
+glue	풀, 풀을 바르다, 접착하다
+ruler	자
+scissors	가위
+eraser	지우개
+desk	책상
+chair	의자
+table	탁자
+pencil	연필
+paper	종이
+room	방, 실
+calendar	달력
+printer	인쇄기, 프린터
+envelope	봉투
+folder	폴더, 서류철
+call	통화, 전화를 걸다
+letter	편지
+seal	도장, 인감, 봉인, 날인하다, 봉하다
+clip	클립, 자르다, 깎다
+pin	핀, 핀으로 고정하다
+message	메시지
+bookcase	책장, 책꽂이
+manager	관리자
+calculator	계산기
+stationery	문구류
+staple	스테이플러로 고정시키다
+punch	(표에 구멍을 뚫는) 펀치, 구멍을 뚫다
+highlighter	형광 컬러 펜
+document	서류, 문서
+printout	출력물
+photocopy	사진 복사물, 사진 복사하다
+
+Day 11 - On the Road
+building	건물
+bakery	제과점
+bank	은행, 둑, 제방
+bookstore	서점
+fire station	소방서
+hospital	병원
+post office	우체국
+museum	박물관
+city hall	시청
+police station	경찰서
+drugstore	약국
+trash	쓰레기
+street	거리
+avenue	도시의 큰 대로, 넓은 길
+block	블록, 덩어리, 장애물, 막다, 봉쇄하다
+straight	똑바른, 직선의, 곧장, 일직선으로
+corner	모퉁이, 구석
+turn	회전, 방향 전환, 돌다
+left	왼쪽
+wrong	잘못된
+pedestrian	보행자
+department store	백화점
+sidewalk	인도, 보도
+crosswalk	횡단보도
+intersection	교차로
+patrol	순찰대, 순찰 경관
+signal	신호, 신호기
+booth	(공중) 전화 박스
+highway	고속도로
+sewer	하수구
+
+Day 12 - Housework & Tools
+iron	다리미, 철, 다림질하다
+lift	올리다, 들어올리다
+wash	세탁물, 씻다
+mop	대걸레, 대걸레로 닦다
+brush	솔, 솔질, 솔질하다
+daily	매일의, 나날의
+hang	걸다, 매달리다
+pick   	골라잡다, 따다
+hammer	망치
+switch	스위치
+hook	갈고리, (물건을 거는) 고리
+ladder	사다리
+carry	나르다
+tool	도구, 연장
+drill	송곳, 구멍을 뚫다
+saw	톱, 톱질하다
+bucket	물통, 양동이
+housework	가사, 집안일
+dig	파다, 파헤치다
+sweep	청소하다, 쓸다
+fold   	개다, 접다
+rake	갈퀴, 긁어모으다
+trim	깎아 다듬다
+polish	닦다, 윤을 내다
+screw	나사, 나사로 죄다, 비틀다
+broom	빗자루
+shovel	삽 
+wrench	렌치(너트를 죄는 기구)
+flashlight	손전등
+outlet	(전기) 콘센트, 출구, 배출구
+
+Day 13 - At the Supermarket
+bottle	병 
+package	(포장용) 용기, 상자, 꾸러미, 소포
+can	깡통, 통조림, (음식물을) 통조림으로 만들다
+box	상자
+tube	튜브, 짜내어 쓰게 된 용기
+item	항목, 품목
+pack 	꾸러미, 한 상자
+bag	자루, 봉지, 가방
+ice	얼음
+bar	막대기, 막대기 모양의 것
+piece	조각
+counter	계산대
+spray	스프레이, 분무기, 뿌리다
+bin	(뚜껑 달린) 큰 상자
+smoked	훈제된
+fresh	신선한
+grain	곡물, 곡류
+vegetable	야채
+cart	손수레, 카트
+seafood	해산물
+cashier	계산원
+freezer	냉동고
+frozen food	냉동식품
+grocery	식료 잡화점, 식품점
+container	용기
+aisle	통로
+dairy	우유의, 유제품의
+bundle	(한 묶음의) 다발, 뭉치
+pile	더미
+cash register	금전 등록기
+
+Day 14 - Describing Things
+long	긴
+short	짧은
+dirty	더러운
+clean	깨끗한, 청소하다, 깨끗이 하다
+high    	높은
+low	낮은
+new	새로운
+open	열린, 열다
+heavy	무거운
+full	가득 찬, 배부른
+dark	어두운
+deep	깊은
+round	둥근, 한 바퀴를 도는
+light	가벼운, (양이) 적은, 빛
+famous	유명한
+colorful	다채로운, 화려한
+empty	텅 빈
+metal	금속
+plastic	플라스틱의, 비닐의, 성형의
+wide	넓은
+tight	꽉 끼는
+loose	헐렁한
+sharp	날카로운
+shallow	얕은
+oval	달걀 모양, 타원형, 달걀 모양의, 타원형의
+square	정사각형, 광장, 정사각형의, 사각의
+crack	(갈라진) 틈, 틈새
+glitter	빛나다
+firm	단단한, 굳은, 회사
+wooden	나무로 만든
+
+Day 15 Senses
+see	보다, 알다, 이해하다
+watch	지켜보다
+look	보다, 바라보다
+hear	듣다
+listen	주의해서 듣다
+smell	냄새, 후각, 냄새가 나다, 냄새를 맡다
+loud	(소리가) 큰, 시끄러운
+bad	불쾌한, 나쁜, (음식이) 상한
+taste	맛, 미각, 맛보다, 맛이 나다
+feel	느끼다
+hard	굳은, 단단한, 어려운
+noise	소음
+bitter	쓴, 쓴맛의
+sweet	달콤한
+sour	신맛의, 신
+juicy	즙이 많은
+touch	촉감, 접촉, 만지다, 건드리다
+rough	거친, 가공하지 않은
+soft	부드러운   
+sense	감각, 감지하다
+objective	객관적인
+sight	시각, 시력, 보기, 일견
+stare	빤히 보다, 응시하다
+whisper	속삭임, 속삭이다
+audio	음의 재생, 오디오, 음성의, 오디오의
+flavor	맛, 풍미
+smooth	부드러운, 부드럽게 하다
+notice	주의, 주목, 주의하다, 알아차리다
+observe 	관찰하다, 알아차리다
+discover	발견하다, 알아내다
+
+Day 16 - Health & Illness
+health	건강
+weak	몸이 약한, 허약한
+sick	(몸이) 아픈, 병든
+cough	기침, 기침하다
+fever	열, 발열
+sore	아픈, 쑤시는
+cut	베인 상처, 상처를 내다, ~을 베다
+pain	고통
+medicine	약
+ache	통증, 아픔, 통증이 있다
+dizzy	어지러운
+disease	병, 질병
+cancer	암
+blind	눈 먼, 시각 장애의
+deaf	귀가 먹은, 청각 장애가 있는
+patient	환자, 끈기 있는, 참을성 있는
+cure	치료법, 치료하다
+relax	쉬게 하다, 편히 쉬다
+burn	화상, 화상을 입다(입히다), (햇볕에) 타다
+symptom	증상
+wound	상처, 상처를 내다
+vomit	토하다
+sneeze	재채기하다
+bruise	멍, 타박상
+examine	진찰하다
+recover	회복하다
+prevent	예방하다, 막다
+medical	의학의
+operate	수술하다
+emergency	비상사태, 응급
+
+Day 17 - Travel
+trip	여행
+airport	공항
+sightseeing	관광
+tourist	여행자, 관광객
+guide	안내자, 가이드, 안내하다
+route	(비행기·기차의) 노선, 경로
+visa	비자, 사증
+flight	비행  
+reserve	예약하다
+cancel	취소, 취소하다
+scenery	풍경
+apply	신청하다 [for]
+passport	여권
+insurance	보험
+reach	도착하다, 도달하다
+attendant	안내원, 시중드는 사람
+board	탑승하다, 승차하다
+depart  	출발하다
+arrive	도착하다   
+land	땅, 착륙하다
+abroad	해외로
+itinerary	여행 일정, 여행 일기
+baggage	짐, 수하물
+claim	요구, 청구, 주장, 요구하다, 주장하다
+check	수표, 점검, 조사하다, 점검하다
+destination	목적지
+security	보안, 안전
+delay	지연, 연기, 연기하다
+jet lag	시차증(여행 시차에 의한 피로·신경 과민)
+souvenir	기념품
+
+Day 18 - Hobbies
+movie	영화
+puzzle	수수께끼, 퍼즐
+game	게임, 시합
+interest	흥미, 관심
+read	읽다
+paint	페인트칠을 하다, 그림을 그리다
+sing	노래를 부르다, 노래하다
+draw   	그림을 그리다
+picture	사진, 그림
+musical	뮤지컬, 음악의
+dance	춤, 춤을 추다
+collect	모으다
+chess	체스, 서양 장기
+hike	하이킹하다, 도보여행하다
+comic	희극의, 만화의
+camp	야영, 캠프, 야영지, 야영하다
+pleasure	기쁨
+stamp	우표, 도장
+jog	조깅하다
+magic	마법, 주술, 마법의
+fix	고치다, 고정시키다
+favorite	가장 좋아하는
+pen pal	편지 친구
+mania	열광
+volunteer	지원자, 자원봉사자
+chat	잡담하다
+model	모형, 모델, 모형의, 모델이 되는
+knit	뜨개질을 하다
+leisure	여가, 한가한 시간, 한가한
+involve	수반하다, 필요로 하다
+
+Day 19 - Sports
+exercise	운동, 연습, 운동하다
+gym	체육관
+stretch	몸을 쭉 뻗다, 쭉 내밀다
+swim	수영하다
+kick	(걷어) 차기, 발길질, 발로 차다
+pass	(공을) 패스하다, 지나가다
+outdoor	집밖의, 실외의
+player	경기자, 선수
+bowling	볼링
+soccer	축구
+goal	(축구의) 골, 득점, 목적, 목표
+shoot	사격, 쏘다, 공을 차다
+coach	코치, 지도자, 마차, 코치하다, 지도하다
+basketball	농구
+baseball	야구
+base  	(야구의) 누, 토대, 기초
+match	시합, 경기, 경쟁 상대
+batter	타자
+throw	투구, 던짐, 던지다
+catch	잡다, 공을 받다
+racket	라켓, 라켓으로 치다
+athlete	운동선수
+defender	수비수
+score	(경기) 득점, 점수, 득점하다
+referee	(운동경기) 심판, 심판을 보다
+champion	챔피언, 우승자
+sweat	땀, (땀이 나도록) 힘든 일, 땀을 흘리다
+dive	잠수, 잠수하다, 물 속에 뛰어들다
+skate	스케이트 구두, 스케이트를 타다
+surf	파도, 파도를 타다, 서핑하다
+
+Day 20 - Shopping
+shop	가게, 물건을 사다
+store	가게, 상점
+gift	선물
+cheap	값싼, 저렴한
+expensive	값비싼
+sale	판매, 염가 판매, 세일
+size	크기
+buy	사다 
+sell	팔다 
+choose	선택하다
+pay	지불하다
+business    	영업
+tax	세금
+mart	시장, 거래소, 슈퍼마켓
+goods	상품, 물품
+tag	정가표
+medium	중간, 중간의
+cash	현금
+change	거스름돈, 변화, 바꾸다
+customer	고객
+display	전시, 진열, 전시하다
+stand	노점, 가판대
+retail	소매
+discount	할인, 할인하다
+receipt	영수증
+brand-name	(유명) 상표가 붙은
+auction	경매
+reasonable	(가격이) 합리적인, 저렴한, 이치에 맞는
+catalog	목록, (상품 등의) 카탈로그
+quality	질, 품질
+
+Day 21 - At the Restaurant
+eat	먹다
+drink	음료, 마실 것, 마시다
+take	받다
+order	주문, 주문하다
+cook	요리사, 요리하다
+chef	요리사, 주방장
+buffet	뷔페 식당, 뷔페식 상차림
+waiter	웨이터
+dessert	디저트, 후식
+napkin	냅킨
+restaurant	음식점
+set	놓다, 준비하다
+deliver	배달하다
+wipe	닦다
+straw	빨대, 지푸라기
+bite	한 입, 물다
+spill	흘리다, 엎지르다
+special	특별한 것, 특별 메뉴, 특별한
+rare	(고기 등이) 덜 익은, 드문, 희귀한
+calorie	칼로리, 열량
+serve	시중들다, 제공하다
+tip	팁, (물건·신체의) 뾰족한 끝
+beverage	음료  
+refill	새 보충물, 다시 채우다, 보충하다
+wrap	싸다, 포장하다, 포장지
+bill	계산서, 청구서
+total	합계, 총액, 전체의, 총
+ingredient	재료
+recommend	추천하다
+appetite	식욕
+
+Day 22 - At the Beach
+sand	모래 
+wave	파도
+shell	조개
+suntan	선탠, 볕에 그을음
+stone	돌
+raft	고무보트, 뗏목
+yacht	요트
+sunglasses	선글라스
+parasol	파라솔, 양산
+mat	돗자리, 매트
+vacation	방학
+rock	바위, 암석
+whistle	호각, 호각을 불다
+lifeboat	구명보트
+scuba	스쿠버, 잠수용 호흡장치
+swimsuit	수영복
+sunblock	자외선 차단제
+cooler	냉장 박스
+blanket	담요
+shade	그늘
+shore	물가, 기슭
+pail	양동이
+sunbath	일광욕
+lifeguard	인명 구조원
+float	물 위에 뜨다, (배 등을) 띄우다
+flipper	물갈퀴, 오리발
+binoculars	쌍안경
+snorkel	스노클을 쓰고 잠수하다, 헤엄치다
+pebble	조약돌, 자갈
+expose	노출하다
+
+Day 23 - Special Days
+festival	축제
+Valentine	밸런타인
+color	색, 색칠하다
+birthday	생일
+card	카드
+candle	초
+blow	끄다
+Christmas	크리스마스
+candy	사탕
+year	해, 년
+wish	소원, 기원하다
+mask	가면
+honeymoon	신혼여행
+Easter	부활절
+hide	숨기다
+invitation	초대, 초대장
+Eve	전날 밤, 이브
+decorate	장식하다
+witch	마녀
+trick	속이다
+costume	복장, 의상
+turkey	칠면조, 칠면조 고기
+congratulate	축하하다
+Thanksgiving	추수 감사절
+Halloween	핼러윈
+reindeer	순록
+lantern	랜턴
+skeleton	해골
+stuff	(속을) 채우다
+crowded 	붐비는, 혼잡한
+
+Day 24 - Outdoor Activities
+jump	점프하다
+seesaw	시소, 시소를 타다
+pond	연못
+ride	탈것, 타다
+bench	긴 의자, 벤치
+event	사건, 행사
+picnic	소풍
+zoo	동물원
+run	달리다
+concert	콘서트, 공연
+visit	방문하다
+rope	줄
+backpack	배낭, 배낭을 지고 걷다
+slide	미끄럼틀, 미끄러지다
+fountain	분수
+playground	놀이터, 운동장
+swing	그네, 그네를 타다
+sleeping bag	침낭
+campfire	모닥불
+fishing rod	낚싯대
+walk	걷다, 산책하다
+amusement	놀이, 즐거움, 재미
+merry-go-round	회전목마
+flea market	벼룩시장
+botanical garden	식물원
+aquarium	수족관
+thermos	보온병
+stake	말뚝
+peak	꼭대기
+rapids	급류, 여울
+
+Day 25 - Nature
+sky	하늘
+flood	홍수
+hurricane	폭풍, 허리케인
+thunder	천둥, 천둥같이 큰 소리
+lightning	번개, 번개 같은
+creature	(신의) 창조물, 피조물
+climb	오르다, 등반하다
+soil	흙, 땅, 토지
+island	섬
+breeze	산들바람
+cliff	벼랑, 절벽
+volcano	화산
+natural	자연의, 자연스러운
+forest	숲, 삼림
+desert	사막
+explore	탐험하다
+stream	시내, 개울, 흐름, 동향
+lake	호수
+waterfall	폭포
+river	강
+ocean	바다, 대양
+coast	해안가
+tide	조수, 간만
+landslide	산사태
+earthquake	지진
+element	요소, 성분, 원소
+appear	나타나다
+source	원천, 수원
+disaster	재난, 재해
+food chain	먹이 사슬
+
+Day 26 - Weather
+hot	더운, 뜨거운
+cold	차가운, 추운
+cool	시원한
+clear	청명한, 깨끗한, 명백한
+sunny	햇빛이 밝은, 화창한
+cloudy	구름 낀
+rainy	비가 오는
+windy	바람 부는
+snowy	눈이 내리는
+foggy	안개 낀
+freezing	몹시 추운, 얼어붙을 듯한
+icy	얼음의, 싸늘한
+dry	건조한, 말리다, 건조하게 하다
+moist	축축한, 습기 있는
+storm	폭풍(우)
+rainfall	강우, 강우량
+hail	우박, 싸락눈
+snowstorm	눈보라
+blizzard	강한 눈보라, 블리자드
+drizzle	이슬비
+melt	녹다, 녹이다
+gale	강풍
+forecast	예측, (일기의) 예보, 예측하다, 예보하다
+condition	상태, 상황
+drought	가뭄
+climate	기후
+degree	(온도 단위인) 도
+temperature	온도
+humid	습한, 눅눅한
+sticky	끈적끈적한, 무더운
+
+Day 27 - Farming
+farm	농장
+fence	울타리
+field	밭
+cowboy	카우보이, 목동
+rabbit	토끼
+sheep	양
+horse	말
+bull	황소
+chicken	닭
+goat	염소
+pig	돼지
+buffalo	버팔로, 물소, 들소
+meadow	목초지, 초원
+barn	헛간
+hay	건초
+harvest	수확하다
+calf	송아지
+cattle	소 
+lay	알을 낳다, ~을 …에 두다
+cotton	면
+shepherd	양치기
+farmhouse	농가, 농가에 딸린 집
+orchard	과수원
+shed	헛간, 광
+ranch	농장, 목축장
+pasture	목장, 방목하다
+combine	콤바인(베어서 탈곡하는 농기계)
+scarecrow	허수아비
+livestock	가축류
+vineyard	포도밭, 포도원
+
+Day 28 - Plants
+flower	꽃
+leaf	나뭇잎, 잎사귀
+bloom	꽃, 개화, 꽃이 피다, 개화하다
+fruit	열매, 과일, 결과, 성과
+grass	풀, 잔디, 잔디밭
+weed	잡초, ~의 잡초를 뽑다
+plant	식물, 심다, 씨를 뿌리다
+seed	씨앗, 종자
+sprout	새싹
+bud	(식물의) 눈, 봉오리
+petal	꽃잎
+root	뿌리, 뿌리 봅다
+stem	(식물의) 줄기, 대, 유래하다, 시작하다
+thorn	가시
+branch	나뭇가지, 분점, 지점
+bough	(나무의) 큰 가지
+maple	단풍나무
+bamboo	대나무
+needle	(침엽수의) 바늘처럼 뾰족한 잎, 바늘
+pine tree	소나무
+cherry tree	벚나무
+cactus	선인장
+trunk	나무의 몸통 부분, 코끼리의 코
+bark	나무껍질, (개가) 짖다
+fertilizer	비료
+bush	관목, 작은 나무들이 우거진 관목 숲
+palm	야자수, 손바닥
+bulb	구근, 알뿌리, 전구
+poisonous	독이 있는
+herb	약초, 허브
+
+Day 29 - Animals
+dog	개
+rat	쥐
+bat	박쥐
+snake	뱀
+elephant	코끼리
+monkey	원숭이
+fox	여우
+tiger	호랑이
+whale	고래
+bear	곰
+deer	사슴
+kangaroo	캥거루
+giraffe	기린
+zebra	얼룩말
+camel	낙타, 낙타색
+dolphin	돌고래
+shark	상어
+leopard	표범
+tortoise	(육지 또는 민물) 거북
+frog	개구리
+dinosaur	공룡
+bird cage	새장
+fish tank	어항
+octopus	문어, 낙지
+jellyfish	해파리
+sea horse	해마
+hippo	하마
+rhino	코뿔소
+crocodile	악어
+lizard	도마뱀
+
+Day 30 - Birds & Insects
+insect	곤충
+ant	개미
+bee	벌
+fly	파리, 날다
+wing	날개
+hen	암탉
+duck	오리
+spider	거미
+butterfly	나비
+beetle	딱정벌레
+ladybug	무당벌레
+flea  	벼룩    
+mosquito	모기
+swan	백조
+penguin	펭귄
+eagle	독수리
+beak	부리
+crow	까마귀
+pigeon	비둘기
+parrot	앵무새
+goose	거위
+cuckoo	뻐꾸기
+owl	올빼미
+peacock	공작새
+ostrich	타조
+swallow	제비, 삼키다
+moth	나방, 좀벌레
+cricket	귀뚜라미
+caterpillar	애벌레, 유충
+hummingbird	벌새
+
+Day 31 - The Environment
+air	공기, 대기
+pollution	오염
+protect	보호하다
+separate	분리하다
+waste	쓰레기, 낭비, 낭비하다
+environment	환경
+effect	영향, 효과
+resource	자원
+recycle	재활용하다
+destroy	파괴하다, 부수다
+damage	손상, 피해
+garbage	쓰레기    
+share	공유하다
+cause	야기하다
+ruin	파괴, 유적,폐허(보통 pl.), 파멸시키다
+raw	날것의, 가공하지 않은
+electricity	전기
+pure	순수한
+smog	스모그, 연무
+fuel	연료
+fossil	화석
+acid	산, 산성의
+toxic	유독한
+exhaust	배기가스, 다 써버리다
+shortage	부족, 결핍
+reduce	줄이다
+endangered	멸종위기의, 위험에 처한
+leak	누출, 새다
+overuse	남용, 남용하다
+greenhouse	온실
+
+Day 32 - Science & Technology
+electric	전기의
+invent	발명하다
+machine	기계
+data	자료, 정보
+important	중요한
+cell	세포
+magnet	자석
+e-mail	이메일
+prove	증명하다
+inform	알리다, 통지하다
+experiment	실험, 실험하다
+method	방법
+chemical	화학의, 화학약품
+measure	측정하다
+technology	기술
+inspect	조사하다
+imagine	상상하다
+visible	눈에 보이는
+vacuum	진공, 공백, (진공청소기로) 청소하다
+react	반응하다
+mobile	이동성의
+charge	충전하다
+multiply	곱하다, 증가시키다
+gravity	중력
+browse	검색하다
+device	장치
+delete	삭제하다
+wireless	무선의
+transmit	보내다, 전송하다, 전염시키다
+formula	공식
+
+Day 33 - Space
+earth  	지구
+sun	태양
+moon	달
+star	별, 항성
+planet	행성
+universe	우주
+solar	태양의
+lunar	달의
+crew	(비행기나 배의) 승무원
+rocket	로켓, 로켓으로 쏘아 올리다
+surface	표면
+Mercury	수성, 수은
+Venus	금성
+Mars	화성
+Jupiter	목성
+Saturn	토성
+ring	고리, 반지
+comet	혜성
+telescope	망원경
+milky way	은하수
+space shuttle	우주 왕복선
+space station	우주 정거장
+eclipse	식(蝕)
+satellite	위성
+orbit	궤도, 궤도를 그리며 돌다
+galaxy	은하수
+astronomy	천문학
+astronomer	천문학자
+Big Bang	빅뱅
+light year	광년
+
+Day 34 - Energy
+power	힘, 권력, 에너지
+energy	에너지
+produce	생산하다, 만들어내다
+oil	기름
+wind 	바람
+coal 	석탄
+mine	광산, 지뢰, 채굴하다
+factory	공장
+dam	댐
+heat	열, 가열하다
+battery	배터리
+generate	생성하다, 발생시키다
+nuclear	원자핵의
+windmill	풍차
+failure	정지, 고장, 실패
+tidal	조수의
+careless	부주의한
+transform	바꾸다, 변형시키다
+natural gas	천연 가스
+abundant	풍부한, 풍족한
+utility pole	전신주
+solar collector	태양열 집열기
+transmission tower	송전탑
+radioactive	방사능을 가진
+power line	송전선
+conserve	보전하다, 아끼다, 절약하다
+efficiency	효율
+crisis	위기
+authorized	권한이 부여된
+turbine	터빈, 원동기
+
+Day 35 - Education
+learn	배우다
+lesson	수업, 교훈
+study	연구, 공부하다
+exam	시험
+grade	학년, 등급, 점수, 점수를 매기다
+college	단과대학
+university	종합대학
+elementary	기초의, 기본이 되는
+discuss	토론하다
+explain	설명하다
+memorize	암기하다
+entrance	입장, 입학
+educate	교육하다, 육성하다
+kindergarten	유치원
+graduate	졸업하다
+knowledge	지식
+schoolmate	학교 친구, 동창생
+homeroom teacher	담임 선생님
+submit	제출하다
+academic	학술적인, 학문적인
+instruct	교수하다, 가르치다
+absent	결석한
+attend 	출석하다, 참석하다
+semester	학기
+alternative	대안, 대안의
+lecture	강의
+pupil	학생
+intelligence	지성, 지능
+scholarship	장학금
+encourage	용기를 북돋우다, 격려하다
+
+Day 36 - School
+teacher	선생님
+classroom	교실
+flag	국기, 깃발
+chalk	분필
+map 	지도
+textbook	교과서
+notebook	공책
+partner	짝, 동료, 협력자
+homework	숙제
+math	수학
+classmate	급우, 학급친구
+senior	선배, 손위의, 최고 학년의
+locker	(자물쇠가 달린) 사물함, 로커
+chalk board	칠판
+marker	마커펜
+club	동아리
+hall	강당, 부속회관
+subject	과목, 주제
+project	계획, 기획, 프로젝트
+library	도서관
+P.E.	체육
+hallway	복도
+principal	교장
+counsel	상담, 상담하다, 권고하다
+admit	받아들이다, 인정하다
+auditorium	강당    
+cafeteria	식당, 간이식당
+assignment	과제, 숙제
+laboratory	실습실, 연습실
+bulletin board	게시판
+
+Day 37 - Society & Economy
+rich	부유한
+save	저축하다, 저장하다, 구하다
+spend	(시간·돈을) 쓰다
+value	가치, 값, 소중히 여기다
+local	지역의
+crowd 	군중
+debt	빚, 부채
+salary	급여
+invest	투자하다
+status	지위, 상태
+culture	문화
+citizen	시민, 국민
+public	대중의, 공공의, 공개된
+supply	공급, 공급하다
+demand	수요, 요구하다
+import 	수입, 수입하다
+export	수출, 수출하다
+account	계좌
+employ	고용하다
+individual	개인, 개인의, 개인적인
+relationship	관계, 사이
+tradition	전통
+consumer	소비자
+responsibility	책임
+influence	영향, 영향을 미치다
+obstacle	장애, 장애물
+property	재산, 소유물
+moral	도덕, 윤리, 도덕적인
+donate	기부하다
+predict	예측하다, 예언하다
+
+Day 38 Politics & Law
+state	국가, 상태
+vote	투표, 투표하다
+party	정당
+gap	차이, 격차
+court	법정, 안뜰
+justice	정의
+crime	범죄
+murder	살인
+victim	희생자
+argue	논하다, 주장하다
+decide	결정하다
+policy	정책
+illegal	불법적인
+guilty	유죄의, 죄책감의
+innocent	무죄의, 결백한
+majority	대다수, 과반수
+minority	소수, 소수의
+suspect	용의자, 의심하다
+witness	목격자, 증인, 목격하다
+arrest	체포, 체포하다
+candidate	후보자
+government	정부, 통치
+elect 	선출하다
+trial	재판, 시행
+sentence	형벌, 문장, 선고하다
+protest	항의, 항의하다
+compensate	보상하다, 변상하다
+diplomat	외교관
+represent	대표하다, 나타내다
+democracy	민주주의
+
+Day 39 - History & Religion
+peace	평화
+war	전쟁
+century	세기, 100년
+age	나이, 시대, 시기
+battle	싸움, 전투
+pray	기도하다, 기원하다
+heaven	천국, 하늘
+soul	정신, 영혼
+belief	믿음, 신념
+church	교회, 성당
+temple	절, 사원
+attack	공격, 공격하다
+weapon	무기
+empire	제국
+rule	지배, 통치, 규칙, 지배하다, 통치하다
+religious	종교의
+charity	자선, 자애
+faithful	신실한, 성실한
+independence	독립
+revolution	혁명
+Buddhism	불교
+Christianity	기독교
+Hinduism	힌두교
+Islam	이슬람교
+Judaism	유대교
+ancient	고대의, 구식의
+colony	식민지
+civilization	문명
+spiritual	정신적인, 영적인
+ceremony	의식, 예식
+
+Day 40 - The World
+different	다른
+global	세계적인
+future	미래
+race	인종, 민족, 경주
+national	국가의
+control	통제, 통제하다
+fund	기금
+die	죽다
+foreign	외국의
+population	인구
+increase	증가, 증가하다
+decrease	감소, 감소하다
+urban	도시의
+rural	시골의
+region	지역, 지방
+border	국경, 경계
+shocking	충격적인, 놀랄만한
+aid	원조, 지원, 구조
+suffer	(고통 등을) 겪다, 당하다
+hunger	배고픔, 기아
+orphan	고아
+support	원조, 지원, 원조하다, 지지하다
+rescue	구하다
+immigrate	(타국으로부터) 이민을 오다
+native	~출신자, 토착민, 토착의, 타고난
+ethnic	민족의, 인종의
+organization	기구
+statistic	통계치
+agreement	협정, 계약, 동의, 합의
+mutual	서로의, 상호의
 `;
